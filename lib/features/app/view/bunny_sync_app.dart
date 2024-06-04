45957aecd9abe4fed9217b765ecb43fc49d73948
Mohamad Tayan <raghad.tayan@gmail.com>
2024-06-05 01:57:13 +0300

    Creating app feature.

---
+++ lib/features/app/view/bunny_sync_app.dart
@@ -0,0 +1,29 @@
+import 'package:bunny_sync/features/app/view/bunny_sync_material_app.dart';
+import 'package:bunny_sync/features/authentication/bloc/authentication_bloc.dart';
+import 'package:bunny_sync/global/di/di.dart';
+import 'package:bunny_sync/global/repos/repos.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BunnySyncApp extends StatelessWidget {
+  const BunnySyncApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider(
+          create: (_) => get<UserRepo>(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (_) => get<AuthenticationBloc>(),
+          ),
+        ],
+        child: const BunnySyncMaterialApp(),
+      ),
+    );
+  }
+}
---
+++ lib/features/app/view/bunny_sync_material_app.dart
@@ -0,0 +1,36 @@
+import 'package:bunny_sync/global/localization/strings.dart';
+import 'package:bunny_sync/global/router/router.dart';
+import 'package:bunny_sync/global/theme/light_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BunnySyncMaterialApp extends StatefulWidget {
+  const BunnySyncMaterialApp({super.key});
+
+  @override
+  State<BunnySyncMaterialApp> createState() => _BunnySyncMaterialAppState();
+}
+
+class _BunnySyncMaterialAppState extends State<BunnySyncMaterialApp> {
+  final AppRouter appRouter = AppRouter();
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      builder: (context, child) {
+        return MaterialApp.router(
+          title: Strings.appName,
+          theme: lightTheme,
+          debugShowCheckedModeBanner: false,
+          routerDelegate: appRouter.delegate(
+            navigatorObservers: () => [
+              HeroController(),
+            ],
+          ),
+          routeInformationParser: appRouter.defaultRouteParser(),
+        );
+      },
+    );
+  }
+}
