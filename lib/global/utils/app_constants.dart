68578b005cdd29b1bd516ff40f59aee212c859a0
Mohamad Tayan <raghad.tayan@gmail.com>
2024-06-19 11:32:46 +0300

    Constants

--- lib/global/utils/app_constants.dart
+++ lib/global/utils/app_constants.dart
@@ -2,7 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 abstract class AppConstants {
-  static const radius = 8.0;
+  static const value8 = 8.0;
+  static const value12 = 12.0;
   static const imageRadius = 8.0;
 
   static const http = 'http';
@@ -11,17 +12,19 @@ abstract class AppConstants {
   static final borderRadius5 = BorderRadius.circular(5);
   static final borderRadius8 = BorderRadius.circular(8);
   static final borderRadius10 = BorderRadius.circular(10);
+  static final borderRadius12 = BorderRadius.circular(12);
   static final borderRadius15 = BorderRadius.circular(15);
+  static const borderRadius18 = BorderRadius.all(Radius.circular(18));
   static final borderRadius20 = BorderRadius.circular(20);
   static final circularBorderRadius = BorderRadius.circular(100);
 
   static const topCornersBorderRadius = BorderRadius.only(
-  topRight: Radius.circular(15),
-  topLeft: Radius.circular(15),
+    topRight: Radius.circular(15),
+    topLeft: Radius.circular(15),
   );
   static const bottomCornersBorderRadius = BorderRadius.only(
-  bottomRight: Radius.circular(15),
-  bottomLeft: Radius.circular(15),
+    bottomRight: Radius.circular(15),
+    bottomLeft: Radius.circular(15),
   );
 
   static const radius20 = Radius.circular(20);
@@ -38,30 +41,37 @@ abstract class AppConstants {
 
   static const searchHeroTag = 'searchHeroTag';
 
-  static const padding16 = EdgeInsets.only(
-  left: 16,
-  top: 16,
-  right: 16,
-  bottom: 16,
+  static const padding16 = EdgeInsets.all(16);
+
+  static const paddingHorizontal16 = EdgeInsets.symmetric(horizontal: 16);
+
+  static const paddingH16V10 = EdgeInsets.symmetric(
+    horizontal: 16,
+    vertical: 10,
   );
 
+  static const paddingH16V20 = EdgeInsets.symmetric(
+    horizontal: 16,
+    vertical: 20,
+  );
+
+  static const paddingHorizontal28 = EdgeInsets.symmetric(horizontal: 28);
+
   static const paddingWithoutTop16 = EdgeInsets.only(
-  left: 16,
-  right: 16,
-  bottom: 16,
+    left: 16,
+    right: 16,
+    bottom: 16,
   );
 
   static const dialogPadding = EdgeInsets.only(
-  left: 16,
-  top: 16,
-  right: 16,
-  bottom: 24,
+    left: 16,
+    top: 16,
+    right: 16,
+    bottom: 24,
   );
 
   static const textFieldContentPadding = EdgeInsets.symmetric(
-  horizontal: 16,
-  vertical: 14,
+    horizontal: 16,
+    vertical: 9,
   );
-
-  static const phoneNumberPrefix = '09';
 }
