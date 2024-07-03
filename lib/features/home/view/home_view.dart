e5f0344a9b5c7757e37778914b9e0f569c262764
Mohamad Tayan <raghad.tayan@gmail.com>
2024-07-04 02:06:54 +0300

    Show loading for logout icon.

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -7,6 +7,7 @@ import 'package:bunny_sync/global/extensions/date_time_x.dart';
 import 'package:bunny_sync/global/gen/assets.gen.dart';
 import 'package:bunny_sync/global/localization/localization.dart';
 import 'package:bunny_sync/global/theme/components/app_colors.dart';
+import 'package:bunny_sync/global/theme/theme.dart';
 import 'package:bunny_sync/global/utils/app_constants.dart';
 import 'package:bunny_sync/global/widgets/animation/indexed_grid_scale_fade_animated_tile.dart';
 import 'package:bunny_sync/global/widgets/bunny_logo.dart';
@@ -53,124 +54,116 @@ class _HomePageState extends State<HomePage> implements HomeViewCallBacks {
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<SignInCubit, GeneralSignInState>(
-      listener: (context, state) {
-        if (state is LogOutSuccess) {
-          MainSnackBar.showSuccessMessageBar(
-            context,
-            "logout_success".i18n,
-          );
-        } else if (state is SignInError) {
-          MainSnackBar.showErrorMessageBar(
-            context,
-            state.message,
-          );
-        }
-      },
-      builder: (context, state) {
-        if (state is SignInLoading) {
-          return const Scaffold(
-            body: Center(
-              child: LoadingIndicator(
+    return Scaffold(
+      appBar: MainAppBar(
+        leadingWith: 70,
+        leading: const Center(
+          child: BunnyLogo(
+            size: 40,
+            borderRadius: AppConstants.borderRadius12,
+          ),
+        ),
+        actions: [
+          BlocConsumer<SignInCubit, GeneralSignInState>(
+            listener: (context, state) {
+              if (state is LogOutSuccess) {
+                MainSnackBar.showSuccessMessageBar(
+                  context,
+                  "logout_success".i18n,
+                );
+              } else if (state is SignInError) {
+                MainSnackBar.showErrorMessageBar(
+                  context,
+                  state.message,
+                );
+              }
+            },
+            builder: (context, state) {
+              if (state is SignInLoading) {
+                return LoadingIndicator(
+                  color: context.cs.primary,
+                );
+              }
+              return IconButton(
+                iconSize: 30,
                 color: AppColors.mainColor,
+                onPressed: onLogOutTap,
+                icon: const Icon(Icons.logout_rounded),
+              );
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        top: false,
+        child: Padding(
+          padding: AppConstants.padding16,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateTime.now().formatMMDoYYYY,
               ),
-            ),
-          );
-        } else if (state is GeneralSignUpInitial || state is SignInError) {
-          return Scaffold(
-            appBar: MainAppBar(
-              leadingWith: 70,
-              leading: const Center(
-                child: BunnyLogo(
-                  size: 40,
-                  borderRadius: AppConstants.borderRadius12,
-                ),
+              Text(
+                'dashboard'.i18n,
+                style: Theme.of(context).textTheme.displayLarge,
               ),
-              actions: [
-                IconButton(
-                  iconSize: 30,
-                  color: AppColors.mainColor,
-                  onPressed: onLogOutTap,
-                  icon: const Icon(Icons.logout_rounded),
-                ),
-              ],
-            ),
-            body: SafeArea(
-              top: false,
-              child: Padding(
-                padding: AppConstants.padding16,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+              AnimationLimiter(
+                child: GridView.count(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.only(top: 16),
                   children: [
-                    Text(
-                      DateTime.now().formatMMDoYYYY,
+                    IndexedGridScaleFadeAnimatedTile(
+                      index: 0,
+                      columnCount: 2,
+                      child: HomeTile(
+                        icon: Assets.icons.gendersThin.path,
+                        title: 'active_breeders'.i18n,
+                        subtitle: 'total'.i18n,
+                        secondSubtitle: '11 ${'does'.i18n} 5 ${'bucks'.i18n}',
+                      ),
                     ),
-                    Text(
-                      'dashboard'.i18n,
-                      style: Theme.of(context).textTheme.displayLarge,
+                    IndexedGridScaleFadeAnimatedTile(
+                      index: 1,
+                      columnCount: 2,
+                      child: HomeTile(
+                        icon: Assets.icons.apps.path,
+                        title: 'active_litters'.i18n,
+                        subtitle: 'total'.i18n,
+                        secondSubtitle: '12 ${'kits'.i18n}',
+                      ),
                     ),
-                    AnimationLimiter(
-                      child: GridView.count(
-                        crossAxisCount: 2,
-                        crossAxisSpacing: 16,
-                        mainAxisSpacing: 16,
-                        shrinkWrap: true,
-                        padding: const EdgeInsets.only(top: 16),
-                        children: [
-                          IndexedGridScaleFadeAnimatedTile(
-                            index: 0,
-                            columnCount: 2,
-                            child: HomeTile(
-                              icon: Assets.icons.gendersThin.path,
-                              title: 'active_breeders'.i18n,
-                              subtitle: 'total'.i18n,
-                              secondSubtitle:
-                                  '11 ${'does'.i18n} 5 ${'bucks'.i18n}',
-                            ),
-                          ),
-                          IndexedGridScaleFadeAnimatedTile(
-                            index: 1,
-                            columnCount: 2,
-                            child: HomeTile(
-                              icon: Assets.icons.apps.path,
-                              title: 'active_litters'.i18n,
-                              subtitle: 'total'.i18n,
-                              secondSubtitle: '12 ${'kits'.i18n}',
-                            ),
-                          ),
-                          IndexedGridScaleFadeAnimatedTile(
-                            index: 2,
-                            columnCount: 2,
-                            child: HomeTile(
-                              icon: Assets.icons.cake.path,
-                              title: 'kits_born'.i18n,
-                              subtitle: 'sold'.i18n,
-                              secondSubtitle:
-                                  '11 ${'butchered'.i18n} 5 ${'died'.i18n}',
-                            ),
-                          ),
-                          IndexedGridScaleFadeAnimatedTile(
-                            index: 3,
-                            columnCount: 2,
-                            child: HomeTile(
-                              icon: Assets.icons.roundDollar.path,
-                              title: 'monthly_finances'.i18n,
-                              subtitle: 'income'.i18n,
-                              secondSubtitle: '90\$ ${'expenses'.i18n}',
-                            ),
-                          ),
-                        ],
+                    IndexedGridScaleFadeAnimatedTile(
+                      index: 2,
+                      columnCount: 2,
+                      child: HomeTile(
+                        icon: Assets.icons.cake.path,
+                        title: 'kits_born'.i18n,
+                        subtitle: 'sold'.i18n,
+                        secondSubtitle:
+                            '11 ${'butchered'.i18n} 5 ${'died'.i18n}',
+                      ),
+                    ),
+                    IndexedGridScaleFadeAnimatedTile(
+                      index: 3,
+                      columnCount: 2,
+                      child: HomeTile(
+                        icon: Assets.icons.roundDollar.path,
+                        title: 'monthly_finances'.i18n,
+                        subtitle: 'income'.i18n,
+                        secondSubtitle: '90\$ ${'expenses'.i18n}',
                       ),
                     ),
                   ],
                 ),
               ),
-            ),
-          );
-        } else {
-          return const SizedBox();
-        }
-      },
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
