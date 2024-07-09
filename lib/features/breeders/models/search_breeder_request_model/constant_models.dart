31ffaf083f41cd16bd690299d6170fd6e08e88ba
Mohamad Tayan <raghad.tayan@gmail.com>
2024-07-09 07:41:29 +0300

    Adding new line.

--- lib/features/breeders/models/search_breeder_request_model/constant_models.dart
+++ lib/features/breeders/models/search_breeder_request_model/constant_models.dart
@@ -1,5 +1,3 @@
 import 'package:bunny_sync/features/breeders/models/search_breeder_request_model/order_model/order_model.dart';
 
 const List<OrderModel> orders = [OrderModel(column: "0", dir: "asc")];
-
-
