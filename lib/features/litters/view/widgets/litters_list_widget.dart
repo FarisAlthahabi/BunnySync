ca7fabdc54f5e2d0a530e16adbda988abeae459b
Mohamad Tayan <raghad.tayan@gmail.com>
2024-07-15 16:02:12 +0300

    Imports.

--- lib/features/litters/view/widgets/litters_list_widget.dart
+++ lib/features/litters/view/widgets/litters_list_widget.dart
@@ -1,10 +1,8 @@
-import 'package:bunny_sync/features/litters/cubit/litters_cubit.dart';
 import 'package:bunny_sync/features/litters/models/litter_entry_model/litter_entry_model.dart';
 import 'package:bunny_sync/features/litters/view/widgets/litter_tile.dart';
 import 'package:bunny_sync/global/utils/app_constants.dart';
 import 'package:bunny_sync/global/widgets/animation/indexed_list_slide_fade_animated_tile.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 
 class LittersListWidget extends StatelessWidget {
