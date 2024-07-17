caee5a5b074516fa705bb983caea832099b9611c
unknown <farisalthahabi04@gmail.com>
2024-07-17 19:17:55 +0300

    creating breeder images fake model

---
+++ lib/features/breeder_details/models/breeder_image_model/breeder_image_fake_model.dart
@@ -0,0 +1,12 @@
+import 'package:bunny_sync/features/breeder_details/models/breeder_image_model/breeder_image_model.dart';
+
+final List<BreederImageModel> breederImagesFake = List.generate(
+  5,
+  (index) => BreederImageModel(
+    breederId: 88,
+    id: index + 1,
+    createdAt: DateTime.now(),
+    updatedAt: DateTime.now(),
+    path: 'd5Az6BDyQM-1716167170-rbPhO0dVqk.png',
+  ),
+);
