e953da42e18c6ac70f0fb2971675edccbd314d72
faris golden <eng.farisalthahabi04@gmail.com>
2024-07-25 17:05:00 +0300

    creating model for get breeder by gender api

---
+++ lib/features/breeders/models/breeder_by_gender_model/breeder_by_gender_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'breeder_by_gender_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BreederByGenderModel _$BreederByGenderModelFromJson(
+        Map<String, dynamic> json) =>
+    BreederByGenderModel(
+      id: (json['id'] as num).toInt(),
+      name: json['name'] as String,
+    );
+
+Map<String, dynamic> _$BreederByGenderModelToJson(
+        BreederByGenderModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
