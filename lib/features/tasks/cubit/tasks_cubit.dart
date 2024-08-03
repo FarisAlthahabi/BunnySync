0ae2b072febc2a4e0442eeb27f783dd09edca136
faris golden <eng.farisalthahabi04@gmail.com>
2024-08-03 17:01:16 +0300

    creating tasks cubit and get tasks method

--- lib/features/tasks/cubit/tasks_cubit.dart
+++ lib/features/tasks/cubit/tasks_cubit.dart
@@ -1,8 +1,33 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
+import 'package:bunny_sync/features/tasks/model/task_model/task_model.dart';
+import 'package:bunny_sync/features/tasks/model/tasks_fake_model.dart';
+import 'package:bunny_sync/features/tasks/repo/tasks_repo.dart';
+import 'package:bunny_sync/global/localization/localization.dart';
+import 'package:injectable/injectable.dart';
+import 'package:meta/meta.dart';
 
-part 'tasks_state.dart';
+part 'states/tasks_state.dart';
 
-class TasksCubit extends Cubit<TasksState> {
-  TasksCubit() : super(TasksInitial());
+part 'states/general_tasks_state.dart';
+
+@injectable
+class TasksCubit extends Cubit<GeneralTasksState> {
+  TasksCubit(this._tasksRepo) : super(TasksInitial());
+
+  final TasksRepo _tasksRepo;
+
+  Future<void> getBreeders() async {
+    emit(TasksLoading(fakeTasks));
+    try {
+      final response = await _tasksRepo.getTasks();
+      if (response.isEmpty) {
+        emit(TasksEmpty("no_tasks".i18n));
+      } else {
+        emit(TasksSuccess(response));
+      }
+    } catch (e, s) {
+      addError(e, s);
+      emit(TasksFail(e.toString()));
+    }
+  }
 }
