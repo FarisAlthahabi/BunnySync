4d41c7c07bb7c89d48dc4adff241b274dc596503
Mohamad Tayan <raghad.tayan@gmail.com>
2024-08-11 04:33:07 +0300

    home view changes.

--- lib/global/utils/bunny_sync_json_utils.dart
+++ lib/global/utils/bunny_sync_json_utils.dart
@@ -7,7 +7,7 @@ abstract class BunnySyncJsonUtils {
 
   static String setBreedersImageUrlFromJson(Map<String, dynamic> json) {
     String? path = json['path'] as String?;
-    path ??= json['breeder_images_path'] as String;
+    path ??= json['breeder_images_path'] as String? ?? '';
     return '${baseUrl}breeders/images/$path';
   }
 }
