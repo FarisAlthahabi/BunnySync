e8e97188cc6ec30689bcd5e545f623529082f7ed
Mohamad Tayan <raghad.tayan@gmail.com>
2024-08-15 07:52:21 +0300

    Adding back button double click.

--- lib/features/breeders/models/breeder_by_gender_model/breeder_by_gender_model.dart
+++ lib/features/breeders/models/breeder_by_gender_model/breeder_by_gender_model.dart
@@ -21,7 +21,9 @@ class BreederByGenderModel implements DropDownItemModel {
 
   @override
   final int id;
+
   @override
+  @JsonKey(name: 'name')
   final String displayName;
 
   String toJsonStr() => jsonEncode(toJson());
