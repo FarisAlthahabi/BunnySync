038de36225086fa3c32b71f3da2a2ef357ec1a70
faris golden <eng.farisalthahabi04@gmail.com>
2024-08-26 14:33:20 +0300

    adding litter concerns cubit to main navigation so that sell tab in litter options works

--- lib/features/main_navigation/view/main_navigation_view.dart
+++ lib/features/main_navigation/view/main_navigation_view.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:bunny_sync/features/main_navigation/cubit/main_navigation_cubit.dart';
 import 'package:bunny_sync/global/blocs/kits_cubit/kits_cubit.dart';
+import 'package:bunny_sync/global/blocs/litter_concerns_cubit/cubit/litter_concerns_cubit.dart';
 import 'package:bunny_sync/global/blocs/rabbit_concerns_cubit/rabbit_concerns_cubit.dart';
 import 'package:bunny_sync/global/di/di.dart';
 import 'package:flutter/material.dart';
@@ -19,6 +20,9 @@ class MainNavigationView extends StatelessWidget {
         ),
         BlocProvider(
           create: (context) => get<RabbitConcernsCubit>(),
+        ),
+        BlocProvider(
+          create: (context) => get<LitterConcernsCubit>(),
         ),
         BlocProvider(
           create: (context) => get<KitsCubit>(),
