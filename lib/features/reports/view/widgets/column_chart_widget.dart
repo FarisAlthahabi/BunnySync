fd86c23ca107addae630ca0235b71b8113cea9c4
faris golden <eng.farisalthahabi04@gmail.com>
2024-09-04 12:19:44 +0300

    wrapping charts with skeleton.replace

--- lib/features/reports/view/widgets/column_chart_widget.dart
+++ lib/features/reports/view/widgets/column_chart_widget.dart
@@ -1,5 +1,6 @@
 import 'package:bunny_sync/global/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class ChartModel {
@@ -32,28 +33,31 @@ class _ColumnChartWidgetState extends State<ColumnChartWidget> {
   @override
   Widget build(BuildContext context) {
     if (widget.data.isNotEmpty) {
-      return SfCartesianChart(
-        primaryYAxis: NumericAxis(
-          labelStyle: context.tt.bodyMedium,
-        ),
-        primaryXAxis: CategoryAxis(
-          labelStyle: context.tt.titleMedium,
-        ),
-        series:List<CartesianSeries<ChartModel, String>>.generate(
-          widget.data[0].yAxisProperty.length , (index) => 
-            ColumnSeries<ChartModel, String>(
-            width: 0.3,
-            animationDuration: widget.animationDuration ?? 1500,
-            color: widget.columnColor?[index] ?? context.cs.primary,
-            dataSource: widget.data,
-            xValueMapper: (ChartModel xAxis, ind) =>
-                xAxis.xAxisProperty.replaceAll('"', ''),
-            yValueMapper: (ChartModel yAxis, ind) => yAxis.yAxisProperty[index],
+      return Skeleton.replace(
+        child: SfCartesianChart(
+          primaryYAxis: NumericAxis(
+            labelStyle: context.tt.bodyMedium,
+          ),
+          primaryXAxis: CategoryAxis(
+            labelStyle: context.tt.titleMedium,
+          ),
+          series: List<CartesianSeries<ChartModel, String>>.generate(
+            widget.data[0].yAxisProperty.length,
+            (index) => ColumnSeries<ChartModel, String>(
+              width: 0.3,
+              animationDuration: widget.animationDuration ?? 1500,
+              color: widget.columnColor?[index] ?? context.cs.primary,
+              dataSource: widget.data,
+              xValueMapper: (ChartModel xAxis, ind) =>
+                  xAxis.xAxisProperty.replaceAll('"', ''),
+              yValueMapper: (ChartModel yAxis, ind) =>
+                  yAxis.yAxisProperty[index],
+            ),
           ),
         ),
       );
     } else {
-      return const SfCartesianChart();
+      return const Skeleton.replace(child: SfCartesianChart());
     }
   }
 }
